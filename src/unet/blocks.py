ee1662a00a635581730dbaa4f4c109d18d3b7a34
kilgore92 <ishaan.bits92@gmail.com>
2019-07-26 12:13:52 +0200

    Adding files

---
+++ src/unet/blocks.py
@@ -0,0 +1,137 @@
+"""
+Class definitions for a standard U-Net Up-and Down-sampling blocks
+http://arxiv.org/abs/1505.04597
+
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self,filter_num=64,in_channels=1,use_bn=False):
+        """
+        Instances the Encoder block that forms a part of a U-Net
+        Parameters:
+            in_channels (int): Depth (or number of channels) of the tensor that the block acts on
+            filter_num (int) : Number of filters used in the convolution ops inside the block,
+                               depth of the output of the enc block
+            use_bn (bool) : Batch-norm is performed between convolutions if this flag is True
+
+
+        """
+        super(EncoderBlock,self).__init__()
+        self.use_bn = use_bn
+        self.filter_num = int(filter_num)
+        self.in_channels = int(in_channels)
+
+        self.conv1 = nn.Conv2d(in_channels=self.in_channels,
+                               out_channels=self.filter_num,
+                               kernel_size=3)
+
+        self.conv2 = nn.Conv2d(in_channels=self.filter_num,
+                               out_channels=self.filter_num,
+                               kernel_size=3)
+
+        if self.use_bn is True:
+            self.bn_op = nn.BatchNorm2d(num_features=self.filter_num)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+
+        if self.use_bn is True:
+            x = self.bn_op(x)
+
+        x = F.relu(self.conv2(x))
+
+        if self.use_bn is True:
+            x = self.bn_op(x)
+
+        return x
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self,in_channels,concat_layer_depth,filter_num,interpolate=False,use_bn=False):
+        """
+        Decoder block used in the U-Net
+
+        Parameters:
+            in_channels (int) : Number of channels of the incoming tensor for the upsampling op
+            concat_layer_depth (int) : Number of channels to be concatenated via skip connections
+            filter_num (int) : Number of filters used in convolution, the depth of the output of the dec block
+            interpolate (bool) : Decides if upsampling needs to performed via interpolation or transposed convolution
+            use_bn (bool) : Batch-norm is performed between convolutions if this flag is True
+
+        """
+        # Up-sampling (interpolation or transposed conv) --> EncoderBlock
+        super(DecoderBlock,self).__init__()
+        self.filter_num = int(filter_num)
+        self.in_channels = int(in_channels)
+        self.concat_layer_depth = int(concat_layer_depth)
+
+        if interpolate:
+            # Upsample by interpolation followed by a 1x1 convolution to obtain desired depth
+            self.up_sample = nn.Sequential(nn.Upsample(scale_factor=2,
+                                                       mode='bilinear',
+                                                       align_corners=True),
+
+                                           nn.Conv2d(in_channels=self.in_channels,
+                                                     out_channels=self.in_channels,
+                                                     kernel_size=1)
+                                           )
+
+        else:
+            # Upsample via transposed convolution (know to produce artifacts)
+            self.up_sample = nn.ConvTranspose2d(in_channels=self.in_channels,
+                                                out_channels=self.in_channels,
+                                                kernel_size=2)
+
+        self.down_sample = EncoderBlock(in_channels=self.in_channels+self.concat_layer_depth,
+                                        filter_num=self.filter_num,
+                                        use_bn=use_bn)
+
+
+    def forward(self,x,skip_layer):
+        up_sample_out = F.relu(self.up_sample(x))
+        padded_up_sample_layer = self.pad_before_merge(up_sample_out,skip_layer)
+        merged_out = torch.cat([padded_up_sample_layer,skip_layer],1)
+        out = self.down_sample(merged_out)
+        return out
+
+    @staticmethod
+    def pad_before_merge(up_sample_layer, skip_layer):
+        """
+        Pads the up sampled layer to match the dims (H,W)
+        of the skip layer before concatenation
+        Source: https://github.com/milesial/Pytorch-UNet/blob/master/unet/unet_parts.py
+        Parameters:
+            up_sample_layer (torch.Tensor) : Tensor holding the upsampled layer
+            skip_layer (torch.Tensor) : Tensor holding the skip layer that needs to concatenated
+
+        Returns:
+            padded_up_sample_layer (torch.Tensor) : Padded tensor that can be merged
+
+
+
+        """
+        diffY = skip_layer.size()[2] - up_sample_layer.size()[2]
+        diffX = skip_layer.size()[3] - up_sample_layer.size()[3]
+        padded_up_sample_layer = F.pad(up_sample_layer, (diffX // 2, diffX - diffX//2,diffY // 2, diffY - diffY//2))
+        return padded_up_sample_layer
+
+
+# Prior Block
+
+
+# Posterior Block
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/unet/model.py
@@ -0,0 +1,108 @@
+"""
+A PyTorch Implementation of a U-Net
+http://arxiv.org/abs/1505.04597
+
+Author: Ishaan Bhat
+i.r.bhat@umcutrecht.nl
+
+"""
+import torch.nn as nn
+import torch.nn.functional as F
+from .blocks import *
+from math import pow
+
+class UNet(nn.Module):
+
+    def __init__(self,image_size=128,n_channels=1,base_filter_num=64,num_blocks=4,num_classes=5,use_bn=False):
+        """
+        PyTorch class definition for the U-Net architecture for image segmentation
+
+        Parameters:
+            image_size (int) : Height or width of a square image (assumes image is square)
+            n_channels (int) : Number of image channels (3 for RGB, 1 for grayscale)
+            base_filter_num (int) : Number of filters for the first convolution (doubled for every subsequent block)
+            num_blocks (int) : Number of encoder/decoder blocks
+            num_classes(int) : Number of classes that need to be segmented
+
+        Returns:
+            out (torch.Tensor) : Prediction of the segmentation map
+
+        """
+        super(UNet,self).__init__()
+
+        self.use_bn = use_bn
+        self.output_shape = (int(image_size), int(image_size))
+        self.contracting_path = nn.ModuleList()
+        self.expanding_path = nn.ModuleList()
+
+        self.num_blocks = num_blocks
+        self.n_channels = int(n_channels)
+        self.n_classes = int(num_classes)
+        self.base_filter_num = int(base_filter_num)
+        self.enc_layer_depths = [] # Keep track of the output depths of each encoder block
+
+        for block_id in range(num_blocks):
+            enc_block_filter_num = pow(2,block_id)*self.base_filter_num # Output depth of current encoder stage
+            if block_id == 0:
+                enc_in_channels = self.n_channels
+            else:
+                enc_in_channels = enc_block_filter_num//2
+            self.enc_layer_depths.append(enc_block_filter_num)
+            self.contracting_path.append(EncoderBlock(in_channels=enc_in_channels,
+                                                      filter_num=enc_block_filter_num,
+                                                      use_bn=self.use_bn))
+
+        # Bottleneck layer
+        bottle_neck_filter_num = enc_block_filter_num*2
+        bottle_neck_in_channels = enc_block_filter_num
+        self.bottle_neck_layer = EncoderBlock(filter_num=bottle_neck_filter_num,
+                                              in_channels=bottle_neck_in_channels,
+                                              use_bn=self.use_bn)
+
+        # Decoder Path
+        for block_id in range(num_blocks):
+            dec_in_channels = bottle_neck_filter_num//pow(2, block_id)
+            self.expanding_path.append(DecoderBlock(in_channels=dec_in_channels,
+                                                    filter_num=self.enc_layer_depths[-1-block_id],
+                                                    concat_layer_depth=self.enc_layer_depths[-1-block_id],
+                                                    interpolate=True,
+                                                    use_bn=self.use_bn))
+
+        # Output Layer
+        self.output = nn.Conv2d(in_channels=int(self.enc_layer_depths[0]),
+                                out_channels=self.n_classes,
+                                kernel_size=1)
+
+    def forward(self,x):
+        # Encoder
+        enc_outputs = []
+        for enc_op in self.contracting_path:
+            x = enc_op(x)
+            enc_outputs.append(x)
+            x = nn.MaxPool2d(kernel_size=2)(x)
+
+        # Bottle-neck layer
+        x = self.bottle_neck_layer(x)
+
+        # Decoder
+        for block_id,dec_op in enumerate(self.expanding_path):
+            x = dec_op(x,enc_outputs[-1-block_id])
+
+        # Output
+        x = self.output(x)
+
+        # Interpolate to match the size of seg-map
+        out = F.interpolate(input=x,
+                            size=self.output_shape,
+                            mode='bilinear',
+                            align_corners=True)
+
+        out = F.relu(out)
+
+        return out
+
+
+
+
+
+
